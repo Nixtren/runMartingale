efc043edf2208296b69ba0fe991e332c2ffbfac3
Ivan <nixtren@protonmail.ch>
2016-05-01 19:44:57 +0100

    Revert "Fix indentation"
    This reverts commit 5ba5b7629b72a701f16fd98b222e0ecf1acfaf69.

--- runMartingale.php
+++ runMartingale.php
@@ -2,7 +2,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2016 Nixtren - https://github.com/Nixtren/runMartingale
+Copyright (c) 2016 Nixtren
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
